cea01a00e6d4f2c9f9bf3fe4a4dd47d22e16f99b
Sanjay Jyoti Dutta <sanjaydutta012@gmail.com>
2022-10-29 12:51:35 +0100

    Create Line continuation.py

---
+++ Line continuation.py
@@ -0,0 +1,22 @@
+# Day 1 :  Line continuation characters in Python
+
+
+
+
+print("I know Python and its very easy for everyone")
+
+#In Python, a backslash ( \ ) is a continuation character.
+print("I know Python and \
+its very easy for everyone")
+
+
+
+
+
+
+
+
+
+
+
+
